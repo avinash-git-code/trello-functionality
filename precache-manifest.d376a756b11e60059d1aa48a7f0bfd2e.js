07e1b4d887b4600781ecb8eff782f8fde8a3df7c
avinash-git-code <avinash.c.pal@gmail.com>
2019-09-15 03:08:22 +0530

    Updates

---
+++ precache-manifest.d376a756b11e60059d1aa48a7f0bfd2e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0bb3dfbf3190ecb2a11",
+    "url": "index.html/static/js/runtime~main.d0bb3dfb.js"
+  },
+  {
+    "revision": "0f434fa7c57ff366bb89",
+    "url": "index.html/static/js/main.0f434fa7.chunk.js"
+  },
+  {
+    "revision": "99fda895ba83eb6f1f6c",
+    "url": "index.html/static/js/2.99fda895.chunk.js"
+  },
+  {
+    "revision": "8efc326fd00c82a029c8cf9468ff3703",
+    "url": "index.html/index.html"
+  }
+];
